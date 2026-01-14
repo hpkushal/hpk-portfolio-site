b0dd8aa88f9ca815433f24bda401be274218b1c6
Kushal Parameshwara <kparameshwara@resulta.com>
2026-01-14 10:08:38 -0400

    perf: self-host Google Fonts using fontsource
    - Install @fontsource/inter and @fontsource/dancing-script
    - Import font CSS in index.tsx
    - Remove Google Fonts CDN links from index.html
    - Eliminates 4 external requests to Google servers

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+
+// Self-hosted fonts (eliminates Google Fonts external requests)
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/dancing-script/400.css';
+import '@fontsource/dancing-script/500.css';
+import '@fontsource/dancing-script/600.css';
+import '@fontsource/dancing-script/700.css';
+
 import './index.css';
 import App from './App';
 
